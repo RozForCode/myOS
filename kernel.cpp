d4fb86d4fd4372aa3f80dcbe1f9dc6d129fdd118
Navrose <navrosesinghjohal@gmail.com>
2024-07-13 15:12:30 -0400

    Add tags which contain info in the makefile to tell the compiler it is not working in a operating system.

--- kernel.cpp
+++ kernel.cpp
@@ -2,7 +2,18 @@
 // it should have a special number in the assembly file
 // 0x1badb002
 #include <cinttypes>
-void kernelMain(void *multiboot_structure, unsigned int magicnumber)
+void printf(char *str)
+{
+    unsigned short *videoMemory = (unsigned short *)0xb8000;
+
+    for (int i = 0; str[i] != '\0'; ++i)
+    {
+        videoMemory[i] = (videoMemory[i] & 0xFF00) | str[i];
+    }
+}
+// so that compiler doesn't change the filename
+// and linker can work properly
+extern "C" void kernelMain(void *multiboot_structure, unsigned int magicnumber)
 {
 
     // while(1):
