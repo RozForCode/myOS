66f441f4e9a33107e73223190a76a527082e67bb
Navrose <navrosesinghjohal@gmail.com>
2024-07-13 12:55:51 -0400

    mini kernel made added magic number for the boatloader to recognize kernel

--- kernel.cpp
+++ kernel.cpp
@@ -1,1 +1,9 @@
-// kernel code
+// for boatloader to think it is a kernel
+// it should have a special number in the assembly file
+// 0x1badb002
+#include <cinttypes>
+void kernelMain(void *multiboot_structure, unsigned int magicnumber)
+{
+
+    // while(1):
+}
