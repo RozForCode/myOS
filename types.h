6c62734a647aa2c573845c7ba566fa4254159309
Navrose <navrosesinghjohal@gmail.com>
2024-07-14 23:40:31 -0400

    Making GDT

--- types.h
+++ types.h
@@ -2,7 +2,7 @@
 #define __TYPES_H
 typedef char int8_t;
 typedef unsigned char uint8_t;
-typedef unsigned short uinit16_t;
+typedef unsigned short uint16_t;
 
 typedef int int32_t;
 typedef unsigned int uint32_t;
